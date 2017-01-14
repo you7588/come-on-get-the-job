0109890dd3a4051d9eb59f72c0a1fa96c8bab8a5
you7588 <yo7588@gmail.com>
2017-01-14 10:41:32 +0800

    implement part 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+    before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+    before_action :require_is_admin
 
   def show
     @job = Job.find(params[:id])
@@ -35,6 +36,7 @@ class Admin::JobsController < ApplicationController
       render :edit
     end
   end
+
 
   def destroy
     @job = Job.find(params[:id])
