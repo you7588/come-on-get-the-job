4b15a0e6c061579493256d4581c5391dbadb8061
you7588 <yo7588@gmail.com>
2017-01-15 10:22:13 +0800

    debug the routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
       resources :resumes
     end
   end
-
+  
+  resources :jobs
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
