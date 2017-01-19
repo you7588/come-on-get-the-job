7a36b045a9bd9ff5ffaa26a53fa7816d5c1f7304
you7588 <yo7588@gmail.com>
2017-01-19 21:36:19 +0800

    change

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
 end
 
   resources :jobs
-  root 'jobs#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
